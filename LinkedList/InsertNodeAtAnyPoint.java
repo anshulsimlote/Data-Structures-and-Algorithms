28664ebe84dad0291d8198f484bc086b44a6a4a2
rokuro881999 <anshulsimlote1999@gmail.com>
2024-07-27 13:05:39 +0530

    Insert Node At Any Point

---
+++ LinkedList/InsertNodeAtAnyPoint.java
@@ -0,0 +1,63 @@
+package LinkedList;
+
+public class InsertNodeAtAnyPoint {
+    Node head;
+    public class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next =null;
+        }
+    }
+    
+    public void insertNodeAtPos(int data, int pos){
+        Node node = new Node(data);
+        
+        // If inserting at the head (position 1)
+        if(pos == 1){
+            node.next = head;  // New node points to the old head
+            head = node;       // Head is now the new node
+            return;
+        }
+        
+        Node curNode = head;
+        int count = 1;  // Start count at 1 since pos is 1-based
+        
+        // Traverse the list to find the position
+        while(curNode != null && count < pos - 1){
+            curNode = curNode.next;
+            count++;
+        }
+        
+        // If curNode is not null, insert the node
+        if(curNode != null){
+            node.next = curNode.next;
+            curNode.next = node;
+        } else {
+            // If curNode is null, the position is out of bounds
+            System.out.println("Position out of bounds");
+        }
+    }
+
+    public void print(){
+        Node curNode = head;
+        while(curNode != null){
+            System.out.print(curNode.data+", ");
+            curNode = curNode.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args){
+        System.out.println("Insert Node At Any Point");
+        InsertNodeAtAnyPoint sl = new InsertNodeAtAnyPoint();
+        sl.insertNodeAtPos(1,1);
+        sl.insertNodeAtPos(2,2);
+        sl.print();  // Output: 1, 2,
+        sl.insertNodeAtPos(4,3);
+        sl.print();  // Output: 1, 2, 4,
+        sl.insertNodeAtPos(3,3);
+        sl.print();  // Output: 1, 2, 3, 4,
+    }
+}
