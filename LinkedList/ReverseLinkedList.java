67d15fb46ba95f2d89a6b882ad127a5b55674126
rokuro881999 <anshulsimlote1999@gmail.com>
2024-07-30 12:51:35 +0530

    Reverse Linked List

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,59 @@
+public class ReverseLinkedList {
+    Node head;
+    public class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void print() {
+        System.out.print("List: ");
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Node current = head;
+        Node prev = null, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head =  prev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Reverse Linked List");
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.print();
+        list.reverse();
+        list.print();
+    }
+}
+
